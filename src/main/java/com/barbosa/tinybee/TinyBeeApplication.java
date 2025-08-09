bbf64d0fcd221a34be7e11425e6744b6a6773873
Antonio Barbosa <antonio.barbos4@outlook.com>
2025-08-09 20:00:29 -0300

    chore: Add Maven Wrapper and initial project structure
    Introduces the Maven Wrapper for easier project setup and management.
    
    Sets up a basic Spring Boot application structure, including a POM file with essential dependencies and a sample application class.
    
    Initializes default application properties and includes a test class to ensure the application context loads correctly.

---
+++ src/main/java/com/barbosa/tinybee/TinyBeeApplication.java
@@ -0,0 +1,13 @@
+package com.barbosa.tinybee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TinyBeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TinyBeeApplication.class, args);
+	}
+
+}
